fc63dab14685ccd03bb2c82381194f2ba0f1a1bb
Nicholas Williams <npwilliams@cru.umhb.edu>
2026-04-25 09:35:24 -0500

    Fix database setup and export models for Render

--- database/setup.js
+++ database/setup.js
@@ -1,7 +1,26 @@
-const db = require("./models");
+const { Sequelize } = require("sequelize");
+const path = require("path");
 
-(async () => {
-  await db.sequelize.sync({ force: true });
-  console.log("Database synced.");
-  process.exit();
-})();
+// Create Sequelize instance
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "database.sqlite"),
+  logging: false
+});
+
+// Load models
+const User = require("./models/User")(sequelize);
+const Musician = require("./models/Musician")(sequelize);
+const PracticeSession = require("./models/PracticeSession")(sequelize);
+const Rehearsal = require("./models/Rehearsal")(sequelize);
+const RehearsalAttendance = require("./models/RehearsalAttendance")(sequelize);
+
+// Export everything
+module.exports = {
+  sequelize,
+  User,
+  Musician,
+  PracticeSession,
+  Rehearsal,
+  RehearsalAttendance
+};
